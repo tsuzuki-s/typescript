b3987d8235e80f3d8404f85db7ff3bd05f7644cc
tsuzuki-s <kiraboshi01@kiraboshi.page>
2021-05-13 16:27:06 +0900

    set

---
+++ dist_/index002_set.js
@@ -0,0 +1,15 @@
+"use strict";
+class SetTest {
+    has(value) {
+        return true;
+    }
+    add(value) {
+        return this;
+    }
+}
+class MutableSet extends SetTest {
+    delete(value) {
+        return true;
+    }
+}
+//# sourceMappingURL=index002_set.js.map
